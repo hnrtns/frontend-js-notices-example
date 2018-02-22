9736e5653f03edbef22b8704baba29cb9cc56808
Henri Tanskanen <henri.tanskanen@gmail.com>
2018-02-22 16:43:00 +0200

    Second commit

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,7 +3,7 @@ FictionalApp v4.20
 License: MIT
 Source code available at: https://github.com/lormipsum/fictional
 
-Third-party license information available at licenses.txt and additional-license-info.txt 
+Third-party license information available at licenses.txt 
 */
 
 // The bundled code goes here
